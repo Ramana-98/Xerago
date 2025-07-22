247747d5eb9e86bf4a1cfc5eaba72eba49855c87
Ramana Athmanathan <ramana@xerago.com>
2025-07-22 18:48:20 +0530

    modidy the code

--- src/components/ui/components/Incometracker.tsx
+++ src/components/ui/components/Incometracker.tsx
@@ -103,7 +103,7 @@ export default function IncomeTracker() {
   const currentData = getCurrentData();
 
   return (
-    <Card className="rounded-2xl bg-[#f7f8f9] p-3 sm:p-4 w-full h-100 hover:shadow-lg  hover:-translate-y-1 transition-all duration-200">
+    <Card className="rounded-2xl bg-white p-3 sm:p-4 w-full h-100 hover:shadow-lg  hover:-translate-y-1 transition-all duration-200">
       <CardHeader className="flex flex-col sm:flex-row justify-between items-start sm:items-center pb-2 gap-1 sm:gap-0">
         <div className="w-full sm:w-auto">
           <CardTitle className="text-lg sm:text-xl font-bold flex items-center gap-2">
